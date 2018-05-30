bf8d4e53429e2ee5cf1132ada3bf07f7bf229c3e
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:32:38 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -6,7 +6,7 @@ puts "What is the name of your party"
 party_name = gets.chomp.capitalize
 
 puts "What is the date of the party"
-date = gets.chomp
+date = gets.chomp.string
 
 puts "What is the time of the party"
 time = gets.chomp
@@ -14,3 +14,6 @@ time = gets.chomp
 puts "Who is hosting the party (First Last)"
 host_name = gets.chomp.capitalize
 # Try starting out with puts'ing a string.
+
+puts "Dear #{{guest_name}}, \n \n You are cordially invited to the #{party_name} on #{date} at #{time}. Please
+RSVP no later than #{date-1}. \n \n Sincerely, \n \n #{host_name}"
