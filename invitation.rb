b35414a2de9a3285804840e4eade540065ce91da
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:29:50 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,16 @@
 # Code your prompts here!
+puts "What is the guest name"
+guest_name = gets.chomp.capitalize
 
+puts "What is the name of your party"
+party_name = gets.chomp
+
+puts "What is the date of the party"
+date = gets.chomp
+
+puts "What is the time of the party"
+time = gets.chomp
+
+puts "Who is hosting the party (First Last)"
+host_name = gets.chomp.capitalize
 # Try starting out with puts'ing a string.
