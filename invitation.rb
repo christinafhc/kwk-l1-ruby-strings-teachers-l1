2d2b8f077cdb0cfb5c7e30fc3993fe117d80814a
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:39:13 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,7 +3,7 @@ puts "Hi, you've been invited to a party! What is your name?"
 guest_name = gets.chomp.capitalize
 
 puts "What is the name of your party"
-party_name = gets.chomp.capitalize
+party_name = gets.chomp.titleize
 
 puts "What is the date of the party"
 date = gets.chomp
